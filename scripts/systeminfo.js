38d92d27f2fb03c2e9d778534e91bd8945d41945
Nimil Peethambaran <nimil.p@hotmail.com>
2018-03-26 22:20:40 -0400

    formatted

--- scripts/systeminfo.js
+++ scripts/systeminfo.js
@@ -9,44 +9,24 @@ SystemInfo.prototype.init = function(){
   SystemInfo.prototype.setUpNetwork();
 
   SystemInfo.prototype.getSystemUsage();
-  // diskUsage(function(info){
-  //   console.log('2');
-  //   console.log(info);
-  // });
 }
 SystemInfo.prototype.getSystemUsage = function(info){
 
 
     const totalDisk = document.querySelector('totalSpace');
     const avail = document.querySelector('available');
-  //   var output = {};
+
     diskUsage(function(info){
       const freeDisk = document.querySelector('#freeDisk'),
-       totalDisk = document.querySelector('#totalSpace');
-       let free, total, avail = (info.total.size-info.total.free),available;
-       free=SystemInfo.prototype.readableMemory({value :info.total.free, desc:"Bytes"});
-       total = SystemInfo.prototype.readableMemory({value :info.total.size, desc:"Bytes"});
-       available = SystemInfo.prototype.readableMemory({value :avail, desc:"Bytes"});
+      totalDisk = document.querySelector('#totalSpace');
+      let free, total, avail = (info.total.size-info.total.free),available;
+      free=SystemInfo.prototype.readableMemory({value :info.total.free, desc:"Bytes"});
+      total = SystemInfo.prototype.readableMemory({value :info.total.size, desc:"Bytes"});
+      available = SystemInfo.prototype.readableMemory({value :avail, desc:"Bytes"});
       freeDisk.innerHTML = free.value+" "+free.desc;
       totalDisk.innerHTML = total.value+" "+total.desc;
       document.querySelector('#available').innerHTML = available.value+" "+available.desc;
-      console.log(info);
   });
-
-
-  //   let path = os.platform() == "win32" ? "c:":"/";
-  //   const freeDisk = document.querySelector('freeDisk');
-  //   const totalDisk = document.querySelector('totalSpace');
-  //   const avail = document.querySelector('available');
-  //   try{
-  //     let info = diskUsage.checkSync(path)
-  //     freeDisk.innerHTML = info.free
-  //     totalDisk.innerHTML = info.total;
-  //     avail.innerHTML = info.available;
-  //     console.log(info.free);
-  //   } catch(error){
-  //     console.log(error);
-  //   }
 }
 SystemInfo.prototype.setUpNetwork = function (){
     const network = os.networkInterfaces();
@@ -57,41 +37,39 @@ SystemInfo.prototype.setUpNetwork = function (){
     const ethernet = document.querySelector('#ethernet');
     for( var prop in network){
       if(prop === "en0" || prop === "eth0"){
-      for(var i =0; i< network[prop].length;i++){
-        let property = network[prop][i];
-        if(!(property.scopeid) && property.scopeid !==0){
-          // console.log(JSON.stringify(property));
-            switch (prop) {
-              case "en0":
-                wireless.innerHTML = property.address;
-                wireless.addEventListener('click', function(e){
-                  clipboard.writeText(property.address);
-                  console.log(property.address)
-                  let myNotification = new Notification('System Info', {
-                    body: 'Wireless address '+property.address+' copied to clipboard successfully!',
-                    silent:true
-                  });
-                })
-                break;
-                case "eth0":
-                  ethernet.innerHTML = property.address;
-                  ethernet.addEventListener('click', function(e){
+        for(var i =0; i< network[prop].length;i++){
+          let property = network[prop][i];
+          if(!(property.scopeid) && property.scopeid !==0){
+
+              switch (prop) {
+                case "en0":
+                  wireless.innerHTML = property.address;
+                  wireless.addEventListener('click', function(e){
                     clipboard.writeText(property.address);
-                    console.log(property.address)
                     let myNotification = new Notification('System Info', {
-                      body: 'Ethernet address '+property.address+' copied to clipboard successfully!',
+                      body: 'Wireless address '+property.address+' copied to clipboard successfully!',
                       silent:true
                     });
                   })
                   break;
-              default:
+                  case "eth0":
+                    ethernet.innerHTML = property.address;
+                    ethernet.addEventListener('click', function(e){
+                      clipboard.writeText(property.address);
+                      let myNotification = new Notification('System Info', {
+                        body: 'Ethernet address '+property.address+' copied to clipboard successfully!',
+                        silent:true
+                      });
+                    });
+                    break;
+                default:
 
-            }
+              }
+          }
         }
-      }
-    }
     }
   }
+}
 SystemInfo.prototype.inHours = function (timeObject){
 
     switch (timeObject.times) {
@@ -114,17 +92,14 @@ SystemInfo.prototype.inHours = function (timeObject){
       timeObject.value = parseInt(timeObject.value/60);
       return this.inHours(timeObject)
     }
-  }
-
-
-
+}
 SystemInfo.prototype.setMemory =function ( type){
 
     const memInfo = type=="free"?os.freemem():os.totalmem();
     const ele = document.querySelector("#"+type);
     const mem = this.readableMemory({value :memInfo, desc:"Bytes"})
     ele.innerHTML = mem.value+" "+mem.desc
-  }
+}
 
 SystemInfo.prototype.setUpTimes =function (){
     const memInfo = os.uptime();
@@ -135,7 +110,7 @@ SystemInfo.prototype.setUpTimes =function (){
       time.desc = "Days"
     }
     uptime.innerHTML = parseInt(time.value)+" "+time.desc
-  }
+}
 SystemInfo.prototype.setCPUs =function (){
     const cpusArray = os.cpus();
     if(!cpusArray){
@@ -149,12 +124,7 @@ SystemInfo.prototype.setCPUs =function (){
       ul.appendChild(li);
       li.innerHTML = cpusArray[i].model;
     }
-  }
-  // ipcRenderer.on("mu:data",(e, data)=>{
-  //   // os = data.os;
-  //
-  //   //alert(data.memInfo.free)
-  // });
+}
 SystemInfo.prototype.readableMemory = function (mem){
 
     if(mem.value <1024){
@@ -177,4 +147,4 @@ SystemInfo.prototype.readableMemory = function (mem){
     }
     // mem.desc = mem.desc === "Bytes"? "MB": (mem.desc==="MB"? "GB":mem.desc);
     return this.readableMemory(mem);
-  }
+}
